fc09724466df5a8b07cc53f4d87aeafe13f7b47f
Mike Clarke <mikedclarke109@gmail.com>
2015-06-13 20:03:03 +0100

    added stats

--- ChapterOne/battleship.js
+++ ChapterOne/battleship.js
@@ -9,19 +9,24 @@ var guesses = 0;
 var isSunk = false;
 
 while(!isSunk){
-	guess = prompt("Enter a number between 0-6");
-	guesses++;
+	guess = prompt("Ready aim fire! (Enter a number between 0-6)");
 	
-	if(guess >= 0 && guess <= 6){
-		if(guess == loc1 || guess == loc2 || guess == loc3){
-		hits++;
-		alert("Thats a hit!");
-		} else {
-		alert("Sorry.. Miss");
-		}
-	}else{
+
+	if(guess < 0 || guess > 6){
 		alert("Not vaild number");
+	}else{
+		guesses++;
+		if(guess == loc1 || guess == loc2 || guess == loc3){
+			hits++;
+			alert("Thats a hit!");
+		} else {
+			alert("Sorry.. Miss");
+		}
 	}
 	
 	if(hits == 3){alert("You Win!"); isSunk = true;}
 }
+
+var stats = "You took " + guesses + " guesses to sink the battleship, " +
+			"which means your shooting accuracy was " + (3/guesses);
+alert(stats);
