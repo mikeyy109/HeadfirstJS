5388e15e4d3cc8942e3d7e274c70e2d201a7c7ad
Mike Clarke <mikedclarke109@gmail.com>
2015-06-13 22:57:47 +0100

    cleanup

---
+++ Arrays/arrays.js
@@ -0,0 +1,14 @@
+function pharses(){
+	var words1 = ["24/7", "multi-tier", "30,000 foot", "b-to-b", "win-win"];
+	var words2 = ["empowered", "value-added", "oriented", "focused", "aligned"];
+	var words3 = ["process", "solution", "tipping-point", "strategy", "vision"];
+
+	var rand1 = Math.floor(Math.random() * words1.length);
+	var rand2 = Math.floor(Math.random() * words2.length);
+	var rand3 = Math.floor(Math.random() * words3.length);
+
+	return words1[rand1] + " " + words2[rand2] + " " + words3[rand3];
+}
+
+var phrase = pharses();
+console.log(phrase);
