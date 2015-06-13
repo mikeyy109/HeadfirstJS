f4e88ef9b81af0411dd2936a2b7d10c86f85040d
Mike Clarke <mikedclarke109@gmail.com>
2015-06-13 20:57:01 +0100

    Added Chapter2
    Focus on functions

---
+++ ChapterThree/functions.js
@@ -0,0 +1,12 @@
+function dog(name, weight){
+	if(weight > 20){
+		console.log(name + " says WOOF WOOF!");
+	}else{
+		console.log(name + " says woof woof");
+	}
+}
+
+dog("rover", 23);
+dog("spot", 13);
+dog("spike", 53);
+dog("lady", 17);
